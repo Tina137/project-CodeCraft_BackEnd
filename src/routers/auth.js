7fea0af6b90cc6a9accfc794d9fffc7c87523a0b
Tina Landar <landartina@gmail.com>
2025-11-09 20:04:47 +0200

    fix all 1

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
-import { register, loginUserController } from '../controllers/auth.js';
-import { validateBody, registerSchema, loginSchema } from '../validation/auth.js';
+import * as ctrl from '../controllers/auth.js';
+import {
+  validateBody,
+  registerSchema,
+  loginSchema,
+} from '../validation/auth.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
-router.post('/register', validateBody(registerSchema), register);
-router.post('/login',    validateBody(loginSchema),    loginUserController);
+router.post('/register', validateBody(registerSchema), ctrl.register);
+router.post('/login', validateBody(loginSchema), ctrl.loginUserController);
 router.post('/logout', authenticate, ctrl.logout);
 
 export default router;
